37adc4bb0e62380b16ebf0b6c07d18d65664b5af
Ye <smallyefeng@163.com>
2017-03-30 22:25:34 +0800

    log的具体使用

--- src/main/java/com/imooc/aspect/HttpAspect.java
+++ src/main/java/com/imooc/aspect/HttpAspect.java
@@ -1,12 +1,14 @@
 package com.imooc.aspect;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by yefeng on 17/3/30.
@@ -18,17 +20,36 @@ public class HttpAspect {
 
     private Logger logger = LoggerFactory.getLogger(HttpAspect.class);
 
-    @Pointcut("execution(public * com.imooc.controller.GirlController.girlList(..))")
+    @Pointcut("execution(public * com.imooc.controller.GirlController.*(..))")
     public void log() {
     }
 
     @Before("log()")
-    public void doBefore() {
-        logger.info("111111");
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        logger.info("url={}",request.getRequestURL());
+
+        //method
+        logger.info("ip={}",request.getRemoteAddr());
+
+        //类方法
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
+
+        //参数
+        logger.info("args={}",joinPoint.getArgs());
     }
 
     @After("log()")
     public void doAfter() {
         logger.info("2222222");
+    }
+
+
+    @AfterReturning(returning = "object",pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        logger.info("response={}",object.toString());
     }
 }
